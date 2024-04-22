a07cbdeeb568557181d1965ea4e6668d7c02147d
pu4mane <pu4mane@gmail.com>
2024-04-22 14:46:17 +0300

    t2 done

--- develop/dev02/task.go
+++ develop/dev02/task.go
@@ -1,23 +1,52 @@
 package main
 
+import (
+	"errors"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
 /*
 === Задача на распаковку ===
 
 Создать Go функцию, осуществляющую примитивную распаковку строки, содержащую повторяющиеся символы / руны, например:
-	- "a4bc2d5e" => "aaaabccddddde"
-	- "abcd" => "abcd"
-	- "45" => "" (некорректная строка)
-	- "" => ""
+  - "a4bc2d5e" => "aaaabccddddde"
+  - "abcd" => "abcd"
+  - "45" => "" (некорректная строка)
+  - "" => ""
+
 Дополнительное задание: поддержка escape - последовательностей
-	- qwe\4\5 => qwe45 (*)
-	- qwe\45 => qwe44444 (*)
-	- qwe\\5 => qwe\\\\\ (*)
+  - qwe\4\5 => qwe45 (*)
+  - qwe\45 => qwe44444 (*)
+  - qwe\\5 => qwe\\\\\ (*)
 
 В случае если была передана некорректная строка функция должна возвращать ошибку. Написать unit-тесты.
 
 Функция должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+var ErrInvalidString = errors.New("invalid string")
 
+func Unpack(s string) (string, error) {
+	var res strings.Builder
+	runes := []rune(s)
+
+	for i := 0; i < len(runes); i++ {
+		ch := runes[i]
+
+		if unicode.IsDigit(ch) {
+			return "", ErrInvalidString
+		}
+
+		repeat := 1
+		if i+1 < len(runes) && unicode.IsDigit(runes[i+1]) {
+			repeat, _ = strconv.Atoi(string(runes[i+1]))
+			i++
+		}
+
+		res.WriteString(strings.Repeat(string(ch), repeat))
+	}
+
+	return res.String(), nil
 }
--- develop/dev02/task_test.go
+++ develop/dev02/task_test.go
@@ -1,1 +1,46 @@
 package main
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestUnpack(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{input: "a4bc2d5e", expected: "aaaabccddddde"},
+		{input: "abccd", expected: "abccd"},
+		{input: "", expected: ""},
+		{input: "aaa0b", expected: "aab"},
+		{input: "а1б2в3", expected: "аббввв"},
+		{input: "я9", expected: "яяяяяяяяя"},
+		{input: "สวัสดี", expected: "สวัสดี"},
+		{input: "สวัส4ดี", expected: "สวัสสสสดี"},
+		{input: "🙃0", expected: ""},
+		{input: "🙂9", expected: "🙂🙂🙂🙂🙂🙂🙂🙂🙂"},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.input, func(t *testing.T) {
+			result, err := Unpack(tc.input)
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func TestUnpackInvalidString(t *testing.T) {
+	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	for _, tc := range invalidStrings {
+		tc := tc
+		t.Run(tc, func(t *testing.T) {
+			_, err := Unpack(tc)
+			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
+		})
+	}
+}
